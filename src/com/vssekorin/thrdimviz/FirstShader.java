2ca62c769bec73f4a3bd37191096800f042b3100
Vseslav Sekorin <VsSekorin@gmail.com>
2019-01-12 23:34:24 +0300

    q

--- src/com/vssekorin/thrdimviz/FirstShader.java
+++ src/com/vssekorin/thrdimviz/FirstShader.java
@@ -1,5 +1,6 @@
 package com.vssekorin.thrdimviz;
 
+import static com.vssekorin.thrdimviz.Application.*;
 import static com.vssekorin.thrdimviz.Geometry.*;
 import static com.vssekorin.thrdimviz.Param.*;
 
@@ -9,7 +10,7 @@ public final class FirstShader implements Shader {
     private float[][] Mit;
     private float[][] Mshadow;
     private float[][] uv = new float[2][3];
-    private float[][] tri = new float[3][3];
+    private float[][] triangle = new float[3][3];
 
     public FirstShader(final float[][] m, final float[][] mit, final float[][] mshadow) {
         this.M = m;
@@ -19,49 +20,51 @@ public final class FirstShader implements Shader {
 
     @Override
     public float[] vertex(final int a, final int b) {
-        float[] uvs = Application.model.vt.get(Application.model.f.get(a)[b][1] - 1);
+        float[] uvs = model.vt.get(model.f.get(a)[b][1] - 1);
         uv[0][b] = uvs[0];
         uv[1][b] = uvs[1];
         float[] gl = mult(
             mult(mxViewport, mxProjection, mxModelView),
-            upLength(Application.model.v.get(Application.model.f.get(a)[b][0] - 1), 4, 1)
+            upLength(model.v.get(model.f.get(a)[b][0] - 1), 4, 1)
         );
-        float[] divGl = div(gl, gl[3]);
-        tri[0][b] = divGl[0];
-        tri[1][b] = divGl[1];
-        tri[2][b] = divGl[2];
+        float[] divGl = lowLength(div(gl, gl[3]), 3);
+        triangle[0][b] = divGl[0];
+        triangle[1][b] = divGl[1];
+        triangle[2][b] = divGl[2];
         return gl;
     }
 
     @Override
     public Object[] fragment(float[] a, int b) {
         Object[] result = new Object[2];
-        float[] sbc = mult(Mshadow, upLength(mult(tri, a), 4, 1)); //Координаты в bufferShadow
+        float[] sbc = mult(Mshadow, upLength(mult(triangle, a), 4, 1));
         sbc = div(sbc, sbc[3]);
-        int shadow = bufferShadow[(int)sbc[0] + (int)sbc[1] * size] < sbc[2] ? 1 : 0; //Сравнение z координаты со значением из bufferShadow
-        float[] vUV = mult(uv, a); //интерполяция uv
-        float[] n = normalize(lowLength(mult(Mit, upLength(getNormal(vUV), 4, 1)), 3)); //нормаль
-        float[] l = normalize(lowLength(mult(M, upLength(ligthDir, 4, 1)), 3)); //свет
+        int index = (int) sbc[0] + (int) sbc[1] * size;
+        float shadow = 0.2f + 0.8f * (bufferShadow[index] < sbc[2] ? 1 : 0);
+        float[] vuv = mult(uv, a);
+        float[] n = normalize(lowLength(mult(Mit, upLength(getNormals(vuv), 4, 1)), 3));
+        float[] l = normalize(lowLength(mult(M, upLength(ligthDir, 4, 1)), 3));
         float[] r = normalize(sub(mult(n, dot(n, l) * 2.0f), l));
-        double rzw = Math.pow(r[2], 10000); //Возведение в некоторую большую степень
-        float diff = dot(n, l); // Чтобы убрать красный цвет с картинки надо делать Math.max с 0, но возможно, он и должен быть.
-        int[] color = getComponents(Application.texture.getRGB((int) (vUV[0] * 2844), (int) (vUV[1] * 2844))); //Компоненты цвета из текстуры
+        float rzw = (float) Math.pow(Math.max(r[2], 0.0f), 10000);
+        float diff = Math.max(dot(n, l), 0.0f);
+        int[] c = getComponents(texture.getRGB((int) (vuv[0] * 2844), (int) (vuv[1] * 2844)));
+        int[] color = new int[3];
         for (int i = 0; i < 3; i++) {
-            color[i] = Math.min((int)(color[i] * shadow * (0.5f * diff + 0.5f * rzw)), 255); //От коэффицеинтов ничего не меняется. Формула неправильная с вероятностью 90%.
+            color[i] = (int) Math.min(c[i] * shadow * (diff + 0.6 * rzw), 255);
         }
         result[0] = false;
         result[1] = toColor(color[0], color[1], color[2]);
         return result;
     }
 
-    private float[] getNormal(float[] vuv) {
-        float[] result = new float[3];
-        int rgb = Application.normals.getRGB((int) (vuv[0] * 2844), (int) (vuv[1] * 2844));
-        int[] components = getComponents(rgb);
+    private float[] getNormals(float[] vuv) {
+        int[] components = getComponents(normals.getRGB((int) (vuv[0] * 2844), (int) (vuv[1] * 2844)));
+        float[] normals = new float[3];
         for (int i = 0; i < 3; i++) {
-            result[i] = components[i] / 255.0f * 2.0f - 1.0f;
+//            normals[2-i] = components[i] / 255.0f * 2.0f - 1.0f;
+            normals[i] = components[i] / 255.0f * 2.0f - 1.0f;
         }
-        return result;
+        return normals;
     }
 
     private int[] getComponents(int rgb) {
--- src/com/vssekorin/thrdimviz/SecondShader.java
+++ src/com/vssekorin/thrdimviz/SecondShader.java
@@ -5,23 +5,23 @@ import static com.vssekorin.thrdimviz.Param.*;
 
 public final class SecondShader implements Shader {
 
-    private final float[][] tr = new float[3][3];
+    private final float[][] triangle = new float[3][3];
 
     @Override
     public float[] vertex(int a, int b) {
         float[] gl = upLength(Application.model.v.get(Application.model.f.get(a)[b][0] - 1), 4, 1);
-        gl = Geometry.mult(mult(mxViewport, mxProjection, mxModelView), gl);
+        gl = mult(mult(mxViewport, mxProjection, mxModelView), gl);
         float[] glLow = lowLength(div(gl, gl[3]), 3);
-        tr[0][b] = glLow[0];
-        tr[1][b] = glLow[1];
-        tr[2][b] = glLow[2];
+        triangle[0][b] = glLow[0];
+        triangle[1][b] = glLow[1];
+        triangle[2][b] = glLow[2];
         return gl;
     }
 
     @Override
     public Object[] fragment(float[] a, int b) {
         final Object[] result = new Object[2];
-        float[] vect = Geometry.mult(tr, a);
+        float[] vect = mult(triangle, a);
         float v = vect[2] / depth;
         float intensity = (v > 1.f ? 1.f : (v < 0.f ? 0.f : v));
         result[0] = false;
